3e9c2d41ef51b74753b1e98d1816144bbbfadb7d
mark l chaves <mark@caughtmyeye.cc>
2020-07-25 06:21:01 +0800

    Update cme-login.php
    Fixed the formatting of the code.

--- cme-login.php
+++ cme-login.php
@@ -21,32 +21,32 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Custom CME Branded Login
  */
 function cme_login_logo() { ?>
-    <style type="text/css">
-		.login {
-			background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://marklchaves.files.wordpress.com/2020/01/cewek-strideby-kampong-glam-ed-bw-wb.jpg);
-			background-size: cover;
-			background-position: center;
+    <style>
+	.login {
+		background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://marklchaves.files.wordpress.com/2020/01/cewek-strideby-kampong-glam-ed-bw-wb.jpg);
+		background-size: cover;
+		background-position: center;
     		background-repeat: no-repeat;
     		background-attachment: fixed;
-		}
-		#loginform {
-  			border-radius: 8px;
-			box-shadow: 0 1px 1px rgba(0,0,0,.2);
-		}
+	}
+	#loginform {
+  		border-radius: 8px;
+		box-shadow: 0 1px 1px rgba(0,0,0,.2);
+	}
         #login h1 a, .login h1 a {
-            background-image: url(https://marklchaves.files.wordpress.com/2020/03/siteicon.png);
-        height:250px;
-        width:250px;
-        background-size: 250px 250px;
-        background-repeat: no-repeat;
-        padding-bottom: 10px;
+        	background-image: url(https://marklchaves.files.wordpress.com/2020/03/siteicon.png);
+        	height:250px;
+        	width:250px;
+        	background-size: 250px 250px;
+        	background-repeat: no-repeat;
+        	padding-bottom: 10px;
         }
-		.login #nav a, .login #backtoblog a {
-			color: silver !important;
-		}
-		.login #nav a:hover, .login #backtoblog a:hover {
-			color: whitesmoke !important;
-		}
+	.login #nav a, .login #backtoblog a {
+		color: silver !important;
+	}
+	.login #nav a:hover, .login #backtoblog a:hover {
+		color: whitesmoke !important;
+	}
     </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'cme_login_logo' );
