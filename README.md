4306ef6c737a934ee885b25f5e48d1a6ba7b18d9
mark l chaves <mark@caughtmyeye.cc>
2021-06-30 18:51:36 +0800

    CSS for neumorphic-like form

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # caught my eye custom login plugin for WordPress
 
-Adds a background image to the WordPress admin login page.
+A neumorphic-like custom login page for WordPress.
+
+This plugin will:
+
+1. Adds a background image to the WordPress admin login page.
+2. Make the login form's background colour a semi-transparent black.
 
 ---
 
 ## Installation
 
-1. Upload the plugin [zip file](https://github.com/marklchaves/cme-login/blob/master/dist/cme-login.zip) to the `/wp-content/plugins/cme-login` directory, or install the plugin through the WordPress plugins page directly.
+1. Upload the **contents** of the plugin zip file to the `/wp-content/plugins/cme-login` directory, or install the plugin through your WordPress plugins page.
 1. Activate the plugin through the 'Plugins' page.
+
+---
+
+## Screen captures
+
+![cme login page](assets/cme-login-screen-capture-1080w.jpg)
 
 ---
 
--- cme-login.php
+++ cme-login.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Plugin Name: cme Login
  * Plugin URI: https://www.caughtmyeye.cc/
- * Description: A custom login page.
+ * Description: A neumorphic-like custom login page.
  * Author: caught my eye
  * Author URI: https://www.caughtmyeye.cc/
- * Version: 1.2.0
+ * Version: 1.3.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
@@ -13,45 +14,57 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 /**
  * Custom CME Branded Login
  */
-function cme_login_logo() { ?>
-    <style>
-	.login {
-		background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/wp-content/plugins/cme-login/assets/cewek-strideby-kampong-glam-ed-bw-wb.webp);
-		background-size: cover;
-		background-position: center;
-    		background-repeat: no-repeat;
-    		background-attachment: fixed;
-	}
-	#loginform {
-  		border-radius: 8px;
-		box-shadow: 0 1px 1px rgba(0,0,0,.2);
-	}
-        #login h1 a, .login h1 a {
-        	background-image: url(/wp-content/plugins/cme-login/assets/cme-logo-480x480.png);
-        	height:250px;
-        	width:250px;
-        	background-size: 250px 250px;
-        	background-repeat: no-repeat;
-        	padding-bottom: 10px;
-        }
-	.login #nav a, .login #backtoblog a {
-		color: silver !important;
-	}
-	.login #nav a:hover, .login #backtoblog a:hover {
-		color: whitesmoke !important;
-	}
-    </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'cme_login_logo' );
+function cme_login_logo()
+{ ?>
+	<style>
+		.login {
+			background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/wp-content/plugins/cme-login/assets/cewek-strideby-kampong-glam-ed-bw-wb.webp);
+			background-size: cover;
+			background-position: center;
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+		}
 
-function cme_login_logo_url() {
-    return home_url();
+		#loginform {
+			background: rgba(0, 0, 0, 0.5);
+			border: none;
+			border-radius: 8px;
+			box-shadow: 0 1px 1px rgba(0, 0, 0, .2);
+			color: white;
+		}
+
+		#login h1 a,
+		.login h1 a {
+			background-image: url(/wp-content/plugins/cme-login/assets/cme-logo-480x480.png);
+			height: 250px;
+			width: 250px;
+			background-size: 250px 250px;
+			background-repeat: no-repeat;
+			padding-bottom: 10px;
+		}
+
+		.login #nav a,
+		.login #backtoblog a {
+			color: silver !important;
+		}
+
+		.login #nav a:hover,
+		.login #backtoblog a:hover {
+			color: whitesmoke !important;
+		}
+	</style>
+<?php }
+add_action('login_enqueue_scripts', 'cme_login_logo');
+
+function cme_login_logo_url()
+{
+	return home_url();
 }
-add_filter( 'login_headerurl', 'cme_login_logo_url' );
+add_filter('login_headerurl', 'cme_login_logo_url');
