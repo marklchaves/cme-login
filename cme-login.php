a1c2d03477b9a3c9a8aabe8d1e5ae2ce679381e4
mark l chaves <mark@caughtmyeye.cc>
2022-03-05 10:43:58 +0800

    Custom heading text

--- cme-login.php
+++ cme-login.php
@@ -24,6 +24,12 @@ if (!defined('ABSPATH')) {
 function cme_login_logo()
 { ?>
 	<style>
+		.cmedev-login-message {
+			color: ghostwhite;
+			font-weight: 300;
+			margin: 1% 0;
+			text-align: center;
+		}
 		.login {
 			background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/wp-content/plugins/cme-login/assets/cewek-strideby-kampong-glam-ed-bw-wb.webp);
 			background-size: cover;
@@ -68,3 +74,11 @@ function cme_login_logo_url()
 	return home_url();
 }
 add_filter('login_headerurl', 'cme_login_logo_url');
+
+function cmedev_login_message( $message ) {
+    if ( empty( $message ) ) {
+        return "<h2 class='cmedev-login-message'>powered by caught my eye dev</h2>";
+    } 
+    return $message;
+}
+add_filter( 'login_message', 'cmedev_login_message' );
